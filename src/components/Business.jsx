a50c6d5d26e5dd882c3a644095236d089e18a0b2
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2024-01-21 14:19:47 +0700

    checkpoint

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,53 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { features } from "../constant";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Business = () => {
-  return (
-    <div>
-      
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index === features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full bg-dimBlue ${styles.flexCenter} `}
+    >
+      <img src={icon} alt={title} className="w-[50%] h-[50%] object-contain" />
     </div>
-  )
-}
+    <div className="flex flex-1 flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
-export default Business
+const Business = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={`${styles.heading2} max-w-[800px]`}>
+        Anda urus bisnisnya, <br className="sm:block hidden" /> kami akan
+        menangani urusan keuangan.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>
+        Dengan kartu kredit yang tepat, Anda dapat meningkatkan kehidupan
+        keuangan Anda dengan membangun kredit, mendapatkan reward, dan menghemat
+        uang. Namun, dengan ratusan kartu kredit yang beredar di pasar.
+      </p>
+
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Business;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,61 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    <section
+      id="home"
+      className={` flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div className={`flex-1 flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="diskon" className="h-[32px] w-[32px] " />
+          <p className={`${styles.paragraph}`}>
+            Diskon <span className="text-white font-medium">20% </span>
+            untuk akun <span className="text-white font-medium">1 bulan</span>
+          </p>
+        </div>
 
-export default Hero
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] ss:leading-[100px] leading-[75px] text-[52px] text-white ">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
+          </h1>
+
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[63px] w-full ss:leading-[100px] leading-[75px] text-[52px] text-white ">
+          Payment Method
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Tim ahli kami menggunakan suatu metodologi untuk mengidentifikasi
+          kartu kredit yang kemungkinan besar sesuai dengan kebutuhan Anda. Kami
+          meneliti tingkat persentase tahunan, biaya tahunan.
+        </p>
+      </div>
+
+      <div
+        className={`relative flex-1 flex ${styles.flexCenter} md:my-0 my-10`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="h-[100%] w-[100%] relative z-[5] "
+        />
+
+        <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient "></div>
+        <div className="absolute z-0 w-[80%] h-[80%] rounded-full bottom-40 white__gradient "></div>
+        <div className="absolute z-0 w-[50%] h-[50%] bottom-20 right-20 blue__gradient "></div>
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
